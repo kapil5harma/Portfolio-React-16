b619bdec4d856b6091c005b5af1c675ea86081f7
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-09-02 00:20:49 +0530

    SocialLinks component

---
+++ src/app/SocialLinks/SocialLinks.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const SocialIcons = (props) => {
+  
+  return (
+    <div className="social-icons animate-icons">
+        <a target="_blank" rel="noopener noreferrer" href="https://github.com/kapil5harma/"><i className="fab fa-github"></i></a>
+        <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/kapil5harma"><i className="fab fa-twitter"></i></a>
+        <a target="_blank" rel="noopener noreferrer" href="https://stackoverflow.com/story/kapil5harma"><i className="fab fa-stack-overflow"></i></a>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/kapilsharma1994/"><i className="fab fa-linkedin"></i></a>
+        <a target="_blank" rel="noopener noreferrer" href="https://medium.com/@kapilsharmajmps"><i className="fab fa-medium"></i></a>
+      </div>
+  );
+};
+
+export default SocialIcons;
