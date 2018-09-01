787319e8ae38da1cc04f3ebaeac1a45b7e5825e2
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-09-01 23:41:45 +0530

    Landing Page component

---
+++ src/app/LandingPage/LandingPage.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const LandingPage = (props) => {
+  
+  return (
+    <div style={{ height: '100vh' }} className="landing-page">
+      
+      <main style={{ color: "#fff" }}>
+        
+        <div>
+          <div className="name">Kapil Sharma</div>
+          <div className="tagline">
+            Frontend Developer | JavaScript Enthusiast 
+          </div>
+          
+        </div>
+      </main>
+      
+    </div>
+  );
+};
+
+export default LandingPage;
+
+
+
+// WEBPACK FOOTER //
+// ./src/pages/LandingPage/LandingPage.js
