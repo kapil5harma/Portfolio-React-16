c088302bd1f02ac24ac1f6a0beeac7414a73f985
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-09-01 23:42:04 +0530

    App.js modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import LandingPage from './app/LandingPage/LandingPage';
+import NavBar from './app/NavBar/NavBar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+      <NavBar></NavBar>
+        <LandingPage></LandingPage>
       </div>
     );
   }
