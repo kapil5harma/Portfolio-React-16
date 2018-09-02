3931a2b4be384c1efec8148b30e6e8e5fbdd2564
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-09-02 23:18:32 +0530

    App.js changed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import LandingPage from './app/LandingPage/LandingPage';
 import NavBar from './app/NavBar/NavBar';
+import SocialLinks from './app/SocialLinks/SocialLinks';
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
       <NavBar></NavBar>
         <LandingPage></LandingPage>
+        <SocialLinks></SocialLinks>
       </div>
     );
   }
