0679264ca8d6bb75a2a5c27b23c698741a63c1cf
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-09-04 23:50:07 +0530

    Re-factored App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import LandingPage from './app/LandingPage/LandingPage';
 import NavBar from './app/NavBar/NavBar';
-import SocialLinks from './app/SocialLinks/SocialLinks';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <NavBar></NavBar>
-        <LandingPage></LandingPage>
-        <SocialLinks></SocialLinks>
+        <NavBar className="nav-bar" />
+        <LandingPage className="landing-page" />
       </div>
     );
   }
