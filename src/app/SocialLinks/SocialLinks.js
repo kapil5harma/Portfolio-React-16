8ee46a18064bddc93654c12cb6e6ee67fc9b9766
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-09-04 23:48:52 +0530

    SocialLinks component re-factored using styled components

--- src/app/SocialLinks/SocialLinks.js
+++ src/app/SocialLinks/SocialLinks.js
@@ -1,15 +1,45 @@
 import React from 'react';
+import { Links } from '../styled-components';
 
-const SocialLinks = (props) => {
-  
+const SocialLinks = props => {
   return (
-    <div className="Social Links">
-        <a target="_blank" rel="noopener noreferrer" href="https://github.com/kapil5harma/"><i className="fab fa-github"></i></a>
-        <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/kapil5harma"><i className="fab fa-twitter"></i></a>
-        <a target="_blank" rel="noopener noreferrer" href="https://stackoverflow.com/story/kapil5harma"><i className="fab fa-stack-overflow"></i></a>
-        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/kapilsharma1994/"><i className="fab fa-linkedin"></i></a>
-        <a target="_blank" rel="noopener noreferrer" href="https://medium.com/@kapilsharmajmps"><i className="fab fa-medium"></i></a>
-      </div>
+    <Links className="links">
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/kapil5harma/"
+      >
+        <i className="fab fa-github" />
+      </a>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://twitter.com/kapil5harma"
+      >
+        <i className="fab fa-twitter" />
+      </a>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://stackoverflow.com/story/kapil5harma"
+      >
+        <i className="fab fa-stack-overflow" />
+      </a>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.linkedin.com/in/kapilsharma1994/"
+      >
+        <i className="fab fa-linkedin" />
+      </a>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://medium.com/@kapilsharmajmps"
+      >
+        <i className="fab fa-medium" />
+      </a>
+    </Links>
   );
 };
 
