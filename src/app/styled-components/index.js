30fe42b45026db852cafbad19c8dc7d4ca59fa9a
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-09-04 23:49:47 +0530

    Styled Components: --> Added - Title,  Container --> Modified - Links

--- src/app/styled-components/index.js
+++ src/app/styled-components/index.js
@@ -5,6 +5,10 @@ export const Nav = styled.div.attrs({})`
   top: 0;
   left: 0;
   width: 100%;
+  height: 3rem;
+  display: flex;
+  align-items: center;
+  font-size: 1.2rem;
   background: rgba(30, 30, 30, 0.4);
   color: #eee;
 `;
@@ -15,6 +19,39 @@ export const Menu = styled.div.attrs({})`
   width: 100%;
 `;
 
+export const Container = styled.div.attrs({})`
+  box-sizing: border-box;
+  padding: 4rem 1rem 1rem 1rem;
+  height: 100vh;
+  width: 100vw;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+`;
+
+export const Title = styled.div.attrs({
+  size: props => props.fontSize,
+  weight: props => props.fontWeight,
+  family: props => props.fontFamily,
+  color: props => props.color
+})`
+  font-family: ${props => props.family};
+  font-size: ${props => props.size};
+  font-weight: ${props => props.weight};
+  color: ${props => props.color};
+`;
+
 export const Links = styled.div.attrs({})`
-  padding: 0.5rem;
+  padding: 0.5rem 1.5rem;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  a {
+    text-decoration: none;
+    color: white;
+    font-size: 4rem;
+    padding: 0.5rem 0.75rem;
+  }
 `;
