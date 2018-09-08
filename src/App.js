6f6f76476ead76ad0ec5bbfe39f1936f5c3610d8
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-09-08 23:33:46 +0530

    ClickMe added to App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import LandingPage from './app/LandingPage/LandingPage';
 import NavBar from './app/NavBar/NavBar';
 import About from './app/About/About';
+import { ClickMe } from './app/styled-components';
 
 class App extends Component {
   render() {
@@ -11,6 +12,9 @@ class App extends Component {
         <NavBar className="nav-bar" />
         <LandingPage className="landing-page" />
         <About className="about" />
+        <ClickMe width="10rem" bottom="1.5rem">
+          Click Me!
+        </ClickMe>
       </div>
     );
   }
