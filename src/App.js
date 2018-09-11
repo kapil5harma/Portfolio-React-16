0aeaf55bf00e147efae5b516c31b6b241d1b151f
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-09-11 23:48:51 +0530

    Scrolling on clicking Click Me button

--- src/App.js
+++ src/App.js
@@ -7,7 +7,22 @@ import { ClickMe } from './app/styled-components';
 import Portfolio from './app/Portfolio/Portfolio';
 
 const scrollPage = () => {
-  window.scrollTo(0, 0);
+  // window.scrollTo(0, 0);
+  // console.log('window.scrollY: ', window.scrollY);
+  // console.log('window.innerHeight: ', window.innerHeight);
+  if (window.scrollY % window.innerHeight === 0) {
+    // console.log('1');
+    window.scrollBy(0, window.innerHeight);
+  } else if (window.scrollY < window.innerHeight) {
+    // console.log('2');
+    window.scrollTo(0, window.innerHeight);
+  } else if (
+    window.scrollY > window.innerHeight &&
+    window.scrollY < 2 * window.innerHeight
+  ) {
+    // console.log('3');
+    window.scrollTo(0, 2 * window.innerHeight);
+  }
 };
 
 class App extends Component {
