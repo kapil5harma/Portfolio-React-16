817eacce52397415ed426387b5671dd36c7bf507
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-09-13 00:27:17 +0530

    slide-in, slide-out classes added on scroll of app page.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
 import React, { Component } from 'react';
+
 import './App.css';
+
 import LandingPage from './app/LandingPage/LandingPage';
 import NavBar from './app/NavBar/NavBar';
 import About from './app/About/About';
-import { ClickMe, ToTop } from './app/styled-components';
 import Portfolio from './app/Portfolio/Portfolio';
 
+import { ClickMe, ToTop } from './app/styled-components';
+
 const scrollPage = () => {
-  // window.scrollTo(0, 0);
-  // console.log('window.scrollY: ', window.scrollY);
-  // console.log('window.innerHeight: ', window.innerHeight);
   if (window.scrollY % window.innerHeight === 0) {
-    // console.log('1');
     window.scrollBy(0, window.innerHeight);
   } else if (window.scrollY < window.innerHeight) {
-    // console.log('2');
     window.scrollTo(0, window.innerHeight);
   } else if (
     window.scrollY > window.innerHeight &&
     window.scrollY < 2 * window.innerHeight
   ) {
-    // console.log('3');
     window.scrollTo(0, 2 * window.innerHeight);
   }
+};
+
+// Binding our scroll event handler.
+window.onscroll = () => {
+  // Grab element with id='to-top' to add and remove classes.
+  let toTop = document.getElementById('to-top');
+
+  // Show/Hide 'To Top' on scroll down/up.
+  if (window.scrollY > 90) {
+    toTop.classList.add('slide-in');
+    toTop.classList.remove('slide-out');
+  } else if (window.scrollY < window.oldScroll) {
+    toTop.classList.add('slide-out');
+    toTop.classList.remove('slide-in');
+  }
+  window.oldScroll = window.scrollY;
 };
 
 class App extends Component {
@@ -37,7 +50,7 @@ class App extends Component {
           Click Me!
           <i className="fas fa-chevron-down" />
         </ClickMe>
-        <ToTop className="to-top">
+        <ToTop className="to-top" id="to-top">
           <i className="fas fa-angle-double-up" />
           To Top
         </ToTop>
